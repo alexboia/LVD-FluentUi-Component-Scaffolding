ff34390519ba0911a402e983a90bc985ff6a65aa
Boia Alexandru <alexboia_ro@yahoo.com>
2021-08-18 15:13:07 +0300

    Added basic logging capabilities.

--- index.js
+++ index.js
@@ -2,19 +2,33 @@
 
 const ComponentTemplate = require('./lib/template.js');
 const ComponentPackage = require('./lib/package.js');
+const ComponentLogger = require('./lib/logger.js');
 
 function run() {
+	const logDirectory = './_logs';
+	const logger = new ComponentLogger(logDirectory);
+
 	try {
+		logger.reportNormalProgressStep('Begin package creation...');
+
 		const templateSourceDir = __dirname;
-		const componentTemplate = new ComponentTemplate(templateSourceDir);
+		const componentTemplate = new ComponentTemplate(templateSourceDir, 
+			logger);
 
 		const packageDestinationRootDir = '.';
-		const componentPackage = new ComponentPackage(packageDestinationRootDir, componentTemplate.read());
+		const componentPackage = new ComponentPackage(packageDestinationRootDir, 
+			componentTemplate.read(), 
+			logger);
 
-		componentPackage.create();
+		componentPackage.create(function (success) {
+			if (success) {
+				logger.reportNormalProgressStep('Completed package creation.');
+			} else {
+				logger.reportProgressWarning('The package could not be created.');
+			}
+		});	
 	} catch (e) {
-		console.error('An error occurred while creating the package.');
-		console.error(e);
+		logger.reportProgressError('An error occured while creating the package', e);
 	}
 }
 
