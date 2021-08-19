306beb372ae11e0143fe57916bf476a0b2352b29
Boia Alexandru <alexboia_ro@yahoo.com>
2021-08-19 16:41:02 +0300

    Refactoring. Now using separate logger instances to avoid overlapping of error logging between transports. Package name validation.

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 "use strict";
 
-const ComponentTemplate = require('./lib/template.js');
-const ComponentPackage = require('./lib/package.js');
 const ComponentLogger = require('./lib/logger.js');
+const ComponentPackageBuilder = require('./lib/package-builder.js');
 
 function run() {
-	const logDirectory = './_logs';
-	const logger = new ComponentLogger(logDirectory);
-
+	const logger = _createLogger();
 	try {
-		logger.reportNormalProgressStep('Begin package creation...');
-
-		const templateSourceDir = __dirname;
-		const componentTemplate = new ComponentTemplate(templateSourceDir, 
-			logger);
-
-		const packageDestinationRootDir = './';
-		const componentPackage = new ComponentPackage(packageDestinationRootDir, 
-			componentTemplate.read(), 
-			logger);
-
-		componentPackage.create(function (success) {
-			if (success) {
-				logger.reportNormalProgressStep('Completed package creation.');
-			} else {
-				logger.reportProgressWarning('The package could not be created.');
-			}
-		});	
+		_buildPackage(logger);
 	} catch (e) {
 		logger.reportProgressError('An error occured while creating the package', e);
 	}
+}
+
+function _createLogger() {
+	const logDirectory = './_logs';
+	const serviceName = 'create-fluentui-component';
+	return new ComponentLogger(logDirectory, serviceName);
+}
+
+function _buildPackage(logger) {
+	const componentPackageBuilder = _createPackageBuilder(logger);
+	logger.reportNormalProgressStep('Begin package creation...');
+
+	componentPackageBuilder.create((success) => {
+		if (success) {
+			logger.reportNormalProgressStep('Completed package creation.');
+		} else {
+			logger.reportProgressError('The package could not be created.');
+		}
+	});
+}
+
+function _createPackageBuilder(logger) {
+	const templateSourceDir = __dirname;
+	const packageDestinationRootDir = './';
+
+	return new ComponentPackageBuilder(templateSourceDir,
+		packageDestinationRootDir, 
+		logger);
 }
 
 run();
